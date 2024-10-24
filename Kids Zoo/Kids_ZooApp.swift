2f3a182d1f244257688cf1c7e9c750948fb26438
Khalid Gad <107983594+imkhalidgad@users.noreply.github.com>
2024-10-24 16:03:57 +0300

    Initial Commit

---
+++ Kids Zoo/Kids_ZooApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kids_ZooApp.swift
+//  Kids Zoo
+//
+//  Created by Khalid Gad on 24/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Kids_ZooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
